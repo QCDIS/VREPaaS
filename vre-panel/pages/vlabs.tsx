2afd0cb373b976a7c9737121bbd2b8a37901ef15
Riccardo Bianchi <contervis@gmail.com>
2022-06-14 17:18:23 +0200

    - Added Tailwind plugin to handle multi-line truncation - Added slug generation from vlabs titles

--- vre-panel/pages/vlabs.tsx
+++ vre-panel/pages/vlabs.tsx
@@ -6,6 +6,14 @@ import Link from 'next/link';
 import { getToken } from 'next-auth/jwt';
 import useAuth from './auth/useAuth';
 
+const getSlug = (title: string) => {
+
+    return title
+        .toLowerCase()
+        .replace(/ /g, '-')
+        .replace(/[^\w-]+/g, '');
+}
+
 const VLabs = ({ token }) => {
 
     const isAuthenticated = useAuth(true);
@@ -33,7 +41,7 @@ const VLabs = ({ token }) => {
                     console.log(error)
                 });
         }
-    }, []);
+    }, [isAuthenticated]);
 
     const { status } = useSession({
         required: true,
@@ -53,16 +61,16 @@ const VLabs = ({ token }) => {
                 <div className='flex flex-row'>
                     {vlabs.map((vlab: any) => {
                         return (
-                            <div key={vlab.name} className="max-w-sm rounded overflow-hidden shadow-lg bg-white m-10 w-3/6">
+                            <div key={getSlug(vlab.title)} className="max-w-sm rounded overflow-hidden shadow-lg bg-white m-10 w-3/6">
                                 <img className="w-full h-40 object-cover" src="lab_icon.png" />
+                                <div className="font-bold text-l mb-2 bg-blue-300 text-white p-5">{vlab.title}</div>
                                 <div className="px-6 py-4">
-                                    <div className="font-bold text-l mb-2">{vlab.title}</div>
-                                    <p className="text-gray-700 text-base">
+                                    <p className="text-gray-700 text-base truncate ...">
                                         {vlab.description}
                                     </p>
                                 </div>
                                 <Link href="#" passHref>
-                                    <button className="ml-5 mb-5 bg-blue-300 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded">Join</button>
+                                    <div></div>
                                 </Link>
                             </div>
                         );
@@ -79,8 +87,6 @@ export async function getServerSideProps(context) {
     const { req } = context;
     const secret = process.env.SECRET;
     const token = await getToken({ req, secret });
-
-    // console.log(token);
 
     return {
         props: {

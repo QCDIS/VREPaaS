b4b412cad4c2f804120f37d57360e33bff9c2b37
Riccardo Bianchi <contervis@gmail.com>
2022-06-30 10:18:08 +0200

    - Changed views structure to handle workflows call independently or referred to a specific lab - Added standalone refresh function in the app dashboard for workflows - At every workflows request, the local table is update with the remote Argo state

--- vreapis/workflows/serializers.py
+++ vreapis/workflows/serializers.py
@@ -1,18 +1,33 @@
+from unittest import result
 from .models import Workflow
 from rest_framework import serializers
 
 class WorkflowListSerializer(serializers.ListSerializer):
 
+    def save(self, **kwargs):
+        print("Calling save")
+        return super().save(**kwargs)
+
     def update(self, instances, validated_data):
         
-        instance_hash = {index: instance for index, instance in enumerate(instances)}
+        print("List update called")
 
-        result = [
-            self.child.update(instance_hash[index], attrs)
-            for index, attrs in enumerate(validated_data)
-        ]
+        wf_mapping = {wf.argo_id: wf for wf in instances}
+        data_mapping = {item['argo_id']: item for item in validated_data}
 
-        return result
+        # Perform creations and updates.
+        ret = []
+        for argo_id, data in data_mapping.items():
+            workflow = wf_mapping.get(argo_id, None)
+            if workflow is not None:
+                ret.append(self.child.update(workflow, data))
+
+        # Perform deletions.
+        for argo_id, workflow in wf_mapping.items():
+            if argo_id not in data_mapping:
+                workflow.delete()
+
+        return ret
 
 
 class WorkflowSerializer(serializers.ModelSerializer):

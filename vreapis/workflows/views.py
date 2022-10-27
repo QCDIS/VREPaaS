0770778ebd47fee66cfbb21dc3feb9e407084478
skoulouzis <koulouzis@gmail.com>
2022-10-27 18:07:57 +0200

    added error messages

--- vreapis/workflows/views.py
+++ vreapis/workflows/views.py
@@ -2,7 +2,7 @@ from email.policy import HTTP
 import io
 
 import requests
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, viewsets, status
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -15,6 +15,7 @@ from . import models, serializers
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
 
 class WorkflowViewSet(GetSerializerMixin,
                       mixins.RetrieveModelMixin,
@@ -88,27 +89,29 @@ class WorkflowViewSet(GetSerializerMixin,
         ARGO_API_URL = 'https://lfw-ds001-i022.lifewatch.dev:32443/ess-22-argowf/api/v1/workflows/ess-22'
         ARGO_URL = 'https://lfw-ds001-i022.lifewatch.dev:32443/ess-22-argowf/workflows/ess-22'
 
+        if 'workflow_payload' not in request.data:
+            logger.error('Request did not contain workflow payload')
+            return Response('Request did not contain workflow payload', status=status.HTTP_400_BAD_REQUEST)
+
         workflow = request.data['workflow_payload']
-
-            logger.error('Workflow submission failed: '+resp.reason)
-            self.set_status(resp.status_code)
-            self.write('Workflow submission failed: '+resp.reason)
-            self.write_error('Workflow submission failed: '+resp.reason)
-            self.flush()
-            return
-
+        if 'vlab' not in request.data:
+            logger.error('Request did not contain vlab info')
+            return Response('Request did not contain vlab info', status=status.HTTP_400_BAD_REQUEST)
         vlab_slug = request.data['vlab']
 
-        resp_submit = requests.post(
-            ARGO_API_URL,
-            json=workflow,
-            headers={
-                'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjRqWGNWSVoyelFMRV9TenZ3a19vYUVvR0pYTkxpdS1CWjlXVHA0RnFQdmcifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6Im5hYXZyZWFwaTItdG9rZW4tZ3JnZmQiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoibmFhdnJlYXBpMiIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6ImFmMzFkYWNiLWMxYWMtNDJkYy1hYTAwLTM0ZDY0NGVkNjRiNiIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDpkZWZhdWx0Om5hYXZyZWFwaTIifQ.VWngM11PLYjhUzHPFGQA-fb5JqJjQcNtheIfuDcAHHP0A_rlbQtbWeUPTN_j6mEIm0VTU990-a3hl8LDZeS7iGQknT73TJi8W836zCD6xmJYL9Dnk6JjTfDcLKft7u4auW2_cimdKM60P9psOJyLGGRLYS7tFzXyoEJGPboUrlnmVT03lrzqEUl6Ni1oH0S56HIJBJ89O2GeKhakayKbgesWr3EjELdawbAIBw8pmQE0R0dftddO1tdUXCph_HJkFuiRvt8RqYJiKFL7KogsmwhnSsRlzqncIKAPmKhamtW09E4sNc_Q60QQGmAVmVpaPkCP1jNpY_PRuQMZQmY57g'
-            }
-        )
+        try:
+            resp_submit = requests.post(
+                ARGO_API_URL,
+                json=workflow,
+                headers={
+                    'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjRqWGNWSVoyelFMRV9TenZ3a19vYUVvR0pYTkxpdS1CWjlXVHA0RnFQdmcifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6Im5hYXZyZWFwaTItdG9rZW4tZ3JnZmQiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoibmFhdnJlYXBpMiIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6ImFmMzFkYWNiLWMxYWMtNDJkYy1hYTAwLTM0ZDY0NGVkNjRiNiIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDpkZWZhdWx0Om5hYXZyZWFwaTIifQ.VWngM11PLYjhUzHPFGQA-fb5JqJjQcNtheIfuDcAHHP0A_rlbQtbWeUPTN_j6mEIm0VTU990-a3hl8LDZeS7iGQknT73TJi8W836zCD6xmJYL9Dnk6JjTfDcLKft7u4auW2_cimdKM60P9psOJyLGGRLYS7tFzXyoEJGPboUrlnmVT03lrzqEUl6Ni1oH0S56HIJBJ89O2GeKhakayKbgesWr3EjELdawbAIBw8pmQE0R0dftddO1tdUXCph_HJkFuiRvt8RqYJiKFL7KogsmwhnSsRlzqncIKAPmKhamtW09E4sNc_Q60QQGmAVmVpaPkCP1jNpY_PRuQMZQmY57g'
+                }
+            )
+        except Exception as ex:
+            logger.error(str(ex))
+            return Response('Error while submitting workflow in: '+ARGO_API_URL, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         resp_submit_data = resp_submit.json()
-
         resp_detail = requests.get(
             f"{ARGO_API_URL}/{resp_submit_data['metadata']['name']}",
             json=workflow,
@@ -118,14 +121,21 @@ class WorkflowViewSet(GetSerializerMixin,
         )
 
         resp_detail_data = resp_detail.json()
+        VirtualLab.objects
+        try:
+            logger.info('Querying vlab: ' + vlab_slug)
+            vlab = VirtualLab.objects.get(slug=vlab_slug)
+        except Exception as ex:
+            logger.error(str(ex))
+            return Response('Error while getting vlab: '+vlab_slug, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        vlab = VirtualLab.objects.get(slug=vlab_slug)
-        new_data = {}
+        if not vlab:
+            logger.error('vlab: '+vlab_slug+ ' not found')
+            return Response('vlab: '+vlab_slug+ ' not found', status=status.HTTP_404_NOT_FOUND)
 
-        new_data['argo_id'] = resp_submit_data['metadata']['name']
-        new_data['status'] = f"{resp_detail_data['status']['phase']} - {resp_detail_data['status']['progress']}"
-        new_data['vlab'] = vlab.id
-        new_data['argo_url'] = f"{ARGO_URL}/{resp_detail_data['metadata']['name']}"
+        new_data = {'argo_id': resp_submit_data['metadata']['name'],
+                    'status': f"{resp_detail_data['status']['phase']} - {resp_detail_data['status']['progress']}",
+                    'vlab': vlab.id, 'argo_url': f"{ARGO_URL}/{resp_detail_data['metadata']['name']}"}
 
         new_workflow = serializers.WorkflowSerializer(data=new_data)
 

94ae58ea706fd805f9fedb8ffdfc8ff0043f888f
skoulouzis <koulouzis@gmail.com>
2022-10-27 17:14:57 +0200

    added logging

--- vreapis/workflows/views.py
+++ vreapis/workflows/views.py
@@ -10,16 +10,17 @@ from vreapis.views import GetSerializerMixin
 from . import serializers
 from virtual_labs.models import VirtualLab
 from virtual_labs.serializers import VirtualLabDetailSerializer
-
+import logging
 from . import models, serializers
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 class WorkflowViewSet(GetSerializerMixin,
                       mixins.RetrieveModelMixin,
                       mixins.UpdateModelMixin,
                       mixins.ListModelMixin,
                       viewsets.GenericViewSet):
-
     queryset = models.Workflow.objects.all()
     serializer_class = serializers.WorkflowSerializer
     serializer_action_classes = {
@@ -27,7 +28,7 @@ class WorkflowViewSet(GetSerializerMixin,
     }
 
     def get_queryset(self):
-        
+
         query_params = self.request.query_params
         vlab_slug = query_params.get('vlab_slug', None)
         vlab_query_set = VirtualLab.objects.filter(slug=vlab_slug)
@@ -39,7 +40,6 @@ class WorkflowViewSet(GetSerializerMixin,
 
         else:
             return models.Workflow.objects.all()
-
 
     def list(self, request, *args, **kwargs):
 
@@ -53,7 +53,7 @@ class WorkflowViewSet(GetSerializerMixin,
 
         resp_list = requests.get(
             ARGO_URL,
-            headers = {
+            headers={
                 'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjRqWGNWSVoyelFMRV9TenZ3a19vYUVvR0pYTkxpdS1CWjlXVHA0RnFQdmcifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6Im5hYXZyZWFwaTItdG9rZW4tZ3JnZmQiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoibmFhdnJlYXBpMiIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6ImFmMzFkYWNiLWMxYWMtNDJkYy1hYTAwLTM0ZDY0NGVkNjRiNiIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDpkZWZhdWx0Om5hYXZyZWFwaTIifQ.VWngM11PLYjhUzHPFGQA-fb5JqJjQcNtheIfuDcAHHP0A_rlbQtbWeUPTN_j6mEIm0VTU990-a3hl8LDZeS7iGQknT73TJi8W836zCD6xmJYL9Dnk6JjTfDcLKft7u4auW2_cimdKM60P9psOJyLGGRLYS7tFzXyoEJGPboUrlnmVT03lrzqEUl6Ni1oH0S56HIJBJ89O2GeKhakayKbgesWr3EjELdawbAIBw8pmQE0R0dftddO1tdUXCph_HJkFuiRvt8RqYJiKFL7KogsmwhnSsRlzqncIKAPmKhamtW09E4sNc_Q60QQGmAVmVpaPkCP1jNpY_PRuQMZQmY57g'
             }
         )
@@ -82,7 +82,6 @@ class WorkflowViewSet(GetSerializerMixin,
 
         return super().list(self, request, *args, **kwargs)
 
-
     @action(detail=False, methods=['POST'], name='Submit a workflow')
     def submit(self, request, *args, **kwargs):
 
@@ -90,12 +89,20 @@ class WorkflowViewSet(GetSerializerMixin,
         ARGO_URL = 'https://lfw-ds001-i022.lifewatch.dev:32443/ess-22-argowf/workflows/ess-22'
 
         workflow = request.data['workflow_payload']
+
+            logger.error('Workflow submission failed: '+resp.reason)
+            self.set_status(resp.status_code)
+            self.write('Workflow submission failed: '+resp.reason)
+            self.write_error('Workflow submission failed: '+resp.reason)
+            self.flush()
+            return
+
         vlab_slug = request.data['vlab']
 
         resp_submit = requests.post(
             ARGO_API_URL,
-            json = workflow,
-            headers = {
+            json=workflow,
+            headers={
                 'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjRqWGNWSVoyelFMRV9TenZ3a19vYUVvR0pYTkxpdS1CWjlXVHA0RnFQdmcifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6Im5hYXZyZWFwaTItdG9rZW4tZ3JnZmQiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoibmFhdnJlYXBpMiIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6ImFmMzFkYWNiLWMxYWMtNDJkYy1hYTAwLTM0ZDY0NGVkNjRiNiIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDpkZWZhdWx0Om5hYXZyZWFwaTIifQ.VWngM11PLYjhUzHPFGQA-fb5JqJjQcNtheIfuDcAHHP0A_rlbQtbWeUPTN_j6mEIm0VTU990-a3hl8LDZeS7iGQknT73TJi8W836zCD6xmJYL9Dnk6JjTfDcLKft7u4auW2_cimdKM60P9psOJyLGGRLYS7tFzXyoEJGPboUrlnmVT03lrzqEUl6Ni1oH0S56HIJBJ89O2GeKhakayKbgesWr3EjELdawbAIBw8pmQE0R0dftddO1tdUXCph_HJkFuiRvt8RqYJiKFL7KogsmwhnSsRlzqncIKAPmKhamtW09E4sNc_Q60QQGmAVmVpaPkCP1jNpY_PRuQMZQmY57g'
             }
         )
@@ -104,8 +111,8 @@ class WorkflowViewSet(GetSerializerMixin,
 
         resp_detail = requests.get(
             f"{ARGO_API_URL}/{resp_submit_data['metadata']['name']}",
-            json = workflow,
-            headers = {
+            json=workflow,
+            headers={
                 'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjRqWGNWSVoyelFMRV9TenZ3a19vYUVvR0pYTkxpdS1CWjlXVHA0RnFQdmcifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6Im5hYXZyZWFwaTItdG9rZW4tZ3JnZmQiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoibmFhdnJlYXBpMiIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6ImFmMzFkYWNiLWMxYWMtNDJkYy1hYTAwLTM0ZDY0NGVkNjRiNiIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDpkZWZhdWx0Om5hYXZyZWFwaTIifQ.VWngM11PLYjhUzHPFGQA-fb5JqJjQcNtheIfuDcAHHP0A_rlbQtbWeUPTN_j6mEIm0VTU990-a3hl8LDZeS7iGQknT73TJi8W836zCD6xmJYL9Dnk6JjTfDcLKft7u4auW2_cimdKM60P9psOJyLGGRLYS7tFzXyoEJGPboUrlnmVT03lrzqEUl6Ni1oH0S56HIJBJ89O2GeKhakayKbgesWr3EjELdawbAIBw8pmQE0R0dftddO1tdUXCph_HJkFuiRvt8RqYJiKFL7KogsmwhnSsRlzqncIKAPmKhamtW09E4sNc_Q60QQGmAVmVpaPkCP1jNpY_PRuQMZQmY57g'
             }
         )
@@ -120,7 +127,7 @@ class WorkflowViewSet(GetSerializerMixin,
         new_data['vlab'] = vlab.id
         new_data['argo_url'] = f"{ARGO_URL}/{resp_detail_data['metadata']['name']}"
 
-        new_workflow = serializers.WorkflowSerializer(data = new_data)
+        new_workflow = serializers.WorkflowSerializer(data=new_data)
 
         if new_workflow.is_valid(raise_exception=True):
             new_workflow.save()

dac803d64d42b03e8fb8f8661665ba1f3faadc29
skoulouzis <koulouzis@gmail.com>
2023-02-07 19:42:03 +0100

    added env vars

---
+++ vre-panel/pages/index.tsx
@@ -0,0 +1,108 @@
+import { getToken } from 'next-auth/jwt';
+import { signIn, useSession } from 'next-auth/react';
+import getConfig from 'next/config';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { NewVREDialog } from '../components/NewVREDialog';
+import { Nav } from '../templates/Nav';
+// import useAuth from './auth/useAuth';
+
+const { publicRuntimeConfig } = getConfig()
+
+const getSlug = (title: string) => {
+
+    return title
+        .toLowerCase()
+        .replace(/ /g, '-')
+        .replace(/[^\w-]+/g, '');
+}
+
+const VLabs = () => {
+
+    // const isAuthenticated = useAuth(true);
+    const [isOpen, setIsOpen] = useState(false);
+    const [vlabs, setVlabs] = useState([]);
+
+    useEffect(() => {
+
+        // if (isAuthenticated) {
+
+            // var requestOptions: RequestInit = {
+            //     method: "GET",
+            //     headers: {
+            //         "Authorization": "Bearer: " + token.accessToken
+            //     },
+            // };
+            const vre_api_url = process.env.VRE_API_URL
+            fetch(`${vre_api_url}/vlabs`)
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log(data);
+                    setVlabs(data);
+                })
+                .catch((error) => {
+                    console.log(error)
+                });
+        // }
+    }, []);
+
+    // const { status } = useSession({
+    //     required: true,
+    //     onUnauthenticated() {
+    //         signIn();
+    //     },
+    // })
+
+    // if (status === "loading") {
+    //     return "Loading or not authenticated..."
+    // }
+
+    return (
+        <div>
+            <Nav />
+            <div className='min-h-screen mx-auto bg-gradient-to-b from-sky-100 to-orange-300'>
+                <div className='grid grid-cols-3'>
+                    {vlabs.map((vlab: any) => {
+                        return (
+                            <div key={getSlug(vlab.title)} className="max-w-sm rounded overflow-hidden shadow-lg bg-white m-10">
+                                <Link
+                                    href={{
+                                        pathname: '/vlabs/[slug]',
+                                        query: { slug: vlab.slug }
+                                    }}
+                                    as={`${publicRuntimeConfig.basePath}/vlabs/${vlab.slug}`}
+                                >
+                                    <div>
+                                        <img className="w-72 h-40 object-cover" src={`${publicRuntimeConfig.staticFolder}/market-research.png`} />
+                                        <div className="font-bold text-l mb-2 bg-blue-300 text-white p-5">{vlab.title}</div>
+                                        <div className="px-6 py-4">
+                                            <p className="text-gray-700 text-base truncate ...">
+                                                {vlab.description}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </Link>
+                            </div>
+                        );
+                    })}
+                </div>
+                <NewVREDialog isOpen={isOpen} setIsOpen={setIsOpen} />
+            </div>
+        </div >
+    )
+};
+
+export async function getServerSideProps(context) {
+
+    const { req } = context;
+    const secret = process.env.SECRET;
+    const token = await getToken({ req, secret });
+
+    return {
+        props: {
+            token: token
+        }
+    };
+}
+
+export default VLabs;
---
+++ vre-panel/pages/vlabs/[slug].tsx
@@ -0,0 +1,155 @@
+import { getToken } from "next-auth/jwt";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { Nav } from "../../templates/Nav";
+import useAuth from "../auth/useAuth";
+
+
+const VLabDetails = ({ token }) => {
+
+    const vlabPlaceholder = {
+        title: "Loading ..",
+        slug: "",
+        description: "Loading ..",
+        endpoint: ""
+    }
+    const isAuthenticated = useAuth(true);
+    const router = useRouter();
+    const { slug } = router.query;
+    const [vlab, setVlab] = useState(vlabPlaceholder)
+    const [workflows, setWorkflows] = useState([])
+    const [loadingWorkflow, setLoadingWorkflows] = useState(false)
+
+    const fecthVlab = async () => {
+
+        var requestOptions: RequestInit = {
+            method: "GET",
+            headers: {
+                "Authorization": "Bearer: " + token.accessToken
+            },
+        };
+
+        const url = process.env.NODE_ENV == "production" ? "https://lfw-ds001-i022.lifewatch.dev:32443/vre-api/api" : "http://localhost:8000/api";
+
+        const res = await fetch(`${url}/vlabs/${slug}`, requestOptions);
+        return res.json();
+    }
+
+    const fetchWorkflows = async () => {
+
+        setLoadingWorkflows(true);
+
+        var requestOptions: RequestInit = {
+            method: "GET",
+            headers: {
+                "Authorization": "Bearer: " + token.accessToken
+            },
+        };
+
+        const url = process.env.NODE_ENV == "production" ? "https://lfw-ds001-i022.lifewatch.dev:32443/vre-api/api" : "http://localhost:8000/api";
+        const res = await fetch(`${url}/workflows?vlab_slug=${slug}`, requestOptions);
+
+        setLoadingWorkflows(false);
+
+        return res.json();
+    }
+
+    useEffect(() => {
+        if (isAuthenticated) {
+
+            Promise.all([
+                fecthVlab().then(setVlab),
+                fetchWorkflows().then(setWorkflows)
+            ])
+        }
+    }, [isAuthenticated]);
+
+
+    const getSpinningButtonClass = () => {
+
+        if (loadingWorkflow) {
+            return "w-5 h-5 animate-spin";
+        }
+
+        return "w-5 h-5";
+    }
+
+    return (
+        <div>
+            <Nav />
+            <div className="grid grid-flow-row-dense grid-cols-4 grid-rows-3 gap-4 p-5 min-h-screen mx-auto bg-gradient-to-b from-sky-100 to-orange-300">
+                <div className="row-span-4 col-span-2 shadow-lg bg-white p-10">
+                    <p className="text-4xl font-sans">{vlab.title}</p>
+                    <a target="blank" href={vlab.endpoint}>
+                        <button className="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded mt-5">
+                            Launch
+                        </button>
+                    </a>
+                    <p className="h-64 mt-5 text-justify">{vlab.description}</p>
+                </div>
+                <div className="row-span-2 col-span-2 shadow-lg bg-white p-10">
+                    <p className="text-2xl font-sans">Workflows Runs</p>
+                    <button type="button"
+                        className="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded mt-5 cursor-pointer"
+                        onClick={() => {fetchWorkflows().then(setWorkflows)}}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className={getSpinningButtonClass()} viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd"
+                                d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z"
+                                clipRule="evenodd" />
+                        </svg>
+                    </button>
+                    <table className="table-auto bg-white mt-5">
+                        <thead>
+                            <tr>
+                                <th className="bg-blue-200 border text-left px-8 py-4">Name</th>
+                                <th className="bg-blue-200 border text-left px-8 py-4">Status</th>
+                                <th className="bg-blue-200 border text-left px-8 py-4">Progress</th>
+                            </tr>
+                        </thead>
+                        {vlab != null ? (
+                            <tbody>
+                                {workflows.map((workflow) => {
+                                    return (
+                                        <tr key={workflow['argo_id']} className="odd:bg-gray-100">
+                                            <td className={"border py-2 px-4"}>
+                                                <a className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+                                                    target="blank"
+                                                    href={workflow['argo_url']}>
+                                                    {workflow['argo_id']}
+                                                </a>
+                                            </td>
+                                            <td className={"border py-2 px-4 text-center"}>{workflow['status']}</td>
+                                            <td className={"border py-2 px-4 text-center"}>{workflow['progress']}</td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                        ) : (
+                            <tbody>
+                            </tbody>
+                        )}
+                    </table>
+                </div>
+                <div className="row-span-2 col-span-2 shadow-lg bg-white p-10">
+                    <p className="text-2xl font-sans">Catalogs</p>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default VLabDetails;
+
+export async function getServerSideProps(context) {
+
+    const { req } = context;
+    const secret = process.env.SECRET;
+    const token = await getToken({ req, secret });
+
+    return {
+        props: {
+            token: token
+        }
+    };
+}

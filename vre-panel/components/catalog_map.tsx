490cdbdc8c05cd9378f96a5b1bdca9b600be83dc
Gabriel Pelouze <gabriel@pelouze.net>
2023-02-24 16:41:03 +0100

    render markers and shapes from test WFS

--- vre-panel/components/catalog_map.tsx
+++ vre-panel/components/catalog_map.tsx
@@ -1,9 +1,46 @@
 import {
+    Map,
+    geoJSON,
+    LatLngBounds,
+} from "leaflet";
+import {
     MapContainer,
     TileLayer,
+    useMapEvents,
 } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
 
+async function get_features(api_endpoint: String, bounds: LatLngBounds) {
+    const url = `${api_endpoint}/?format=json&in_bbox=${bounds.toBBoxString()}`;
+    const requestOptions: RequestInit = {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+        }
+        };
+    console.log(url, requestOptions);
+    const res = await fetch(url, requestOptions);
+    return res.json();
+}
+
+async function render_features(api_endpoint: String, map: Map) {
+    const shapes = await get_features(api_endpoint, map.getBounds());
+    geoJSON(shapes)
+        .bindPopup((layer) => layer.feature.properties.name)
+        .addTo(map);
+}
+
+function CatalogItems() {
+    const url = process.env.NEXT_PUBLIC_ENV_WFS_API_URL;
+    const map = useMapEvents({
+        moveend: () => {
+            render_features(`${url}/shapes`, map);
+            render_features(`${url}/markers`, map);
+        },
+    });
+    console.log(map)
+    return null
+}
 
 const CatalogMap = () => {
   return (
@@ -12,6 +49,7 @@ const CatalogMap = () => {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
+          <CatalogItems/>
       </MapContainer>
   )
 }

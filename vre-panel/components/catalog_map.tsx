917504480c1270fee2c8679db5539f56c463b115
Gabriel Pelouze <gabriel@pelouze.net>
2023-04-05 16:09:50 +0200

    connect map viewer data products API

--- vre-panel/components/catalog_map.tsx
+++ vre-panel/components/catalog_map.tsx
@@ -40,8 +40,7 @@ async function render_features(map: Map) {
     geo_json.addTo(map);
 
     const url = process.env.NEXT_PUBLIC_ENV_VRE_API_URL;
-    get_features(`${url}/shapes`, map.getBounds()).then((d) =>geo_json.addData(d));
-    get_features(`${url}/markers`, map.getBounds()).then((d) =>geo_json.addData(d));
+    get_features(`${url}/geodataprods`, map.getBounds()).then((d) =>geo_json.addData(d));
 }
 
 function CallbackHooks() {

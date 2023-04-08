9766e88f804773ec717c3ff360a25ea94df21ecf
skoulouzis <koulouzis@gmail.com>
2023-04-08 21:25:50 +0200

    fix Type error: 'setInterval' is declared but its value is never read.

--- vre-panel/pages/_app.tsx
+++ vre-panel/pages/_app.tsx
@@ -7,7 +7,7 @@ import getConfig from 'next/config'
 export default function App({ Component, pageProps: { session, ...pageProps }} : AppProps): JSX.Element {
 
   const { publicRuntimeConfig } = getConfig()
-  const [interval, setInterval] = useState(0);
+  const [interval] = useState(0);
 
   return (
     <SessionProvider session={session} refetchInterval={interval} basePath={`${publicRuntimeConfig.basePath}/api/auth`}>

5a3eceda7cc25950d2d3fe98b791ee5171609871
skoulouzis <koulouzis@gmail.com>
2023-04-08 22:11:12 +0200

    fixed tsx errors

--- vre-panel/pages/_app.tsx
+++ vre-panel/pages/_app.tsx
@@ -2,11 +2,13 @@ import { AppProps } from 'next/app';
 import '../styles/globals.css';
 import { SessionProvider } from "next-auth/react"
 import { useState } from 'react';
+// import RefreshTokenHandler from './auth/refreshTokenHandler';
 import getConfig from 'next/config'
 
 export default function App({ Component, pageProps: { session, ...pageProps }} : AppProps): JSX.Element {
 
   const { publicRuntimeConfig } = getConfig()
+  // const [interval, setInterval] = useState(0);
   const [interval] = useState(0);
 
   return (

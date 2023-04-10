4e9a2c4c42b5ac25e2bb1601a6aa22971c765f2e
skoulouzis <koulouzis@gmail.com>
2023-04-10 19:56:32 +0200

    npm run build

--- vre-panel/entrypoint.sh
+++ vre-panel/entrypoint.sh
@@ -5,5 +5,8 @@ test -n "$NEXT_PUBLIC_ENV_VRE_API_URL"
 
 find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_ENV_VRE_API_URL#$NEXT_PUBLIC_ENV_VRE_API_URL#g"
 
+
+echo $NEXT_PUBLIC_ENV_VRE_API_URL
+
 echo "Starting Nextjs"
 exec "$@"

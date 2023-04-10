8be8d6e5b8a1b92afdbc7eb7a2a06732a269b197
skoulouzis <koulouzis@gmail.com>
2023-04-10 18:01:09 +0200

    .env

--- vre-panel/entrypoint.sh
+++ vre-panel/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-echo "Check that we have NEXT_PUBLIC_ENV_VRE_API_URL vars"
-test -n "$NEXT_PUBLIC_ENV_VRE_API_URL"
+sed -i 's#=.*$#=$NEXT_PUBLIC_ENV_VRE_API_URL#' .env
 
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_ENV_VRE_API_URL#$NEXT_PUBLIC_ENV_VRE_API_URL#g"
-
-echo "Starting Nextjs"
-exec "$@"
+node server.js

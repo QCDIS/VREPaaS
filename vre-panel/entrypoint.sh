c9370e42328df3ea3282e5896b7c4e1fb9c2ca21
skoulouzis <koulouzis@gmail.com>
2023-04-10 18:53:46 +0200

    run build

--- vre-panel/entrypoint.sh
+++ vre-panel/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 sed  -i "s|NEXT_PUBLIC_ENV_VRE_API_URL=.*$|NEXT_PUBLIC_ENV_VRE_API_URL=${NEXT_PUBLIC_ENV_VRE_API_URL}|" .env
 
-npm install
-npm ci
 
 npm run build
 export NODE_ENV=production

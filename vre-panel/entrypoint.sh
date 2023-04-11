1acaa08670ee2c52b2d7e0d411f9794fd265d5ba
skoulouzis <koulouzis@gmail.com>
2023-04-11 15:06:36 +0200

    simple docker

--- vre-panel/entrypoint.sh
+++ vre-panel/entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
-
-echo "Check that we have NEXT_PUBLIC_ENV_VRE_API_URL vars"
-test -n "$NEXT_PUBLIC_ENV_VRE_API_URL"
-
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_ENV_VRE_API_URL#$NEXT_PUBLIC_ENV_VRE_API_URL#g"
-
-
-echo $NEXT_PUBLIC_ENV_VRE_API_URL
-
-echo "Starting Nextjs"
-exec "$@"
+sed  -i "s|NEXT_PUBLIC_ENV_VRE_API_URL=.*$|NEXT_PUBLIC_ENV_VRE_API_URL=${NEXT_PUBLIC_ENV_VRE_API_URL}|" .env
+cat .env
+ run build
+node server.js
--- vre-panel/next.config.js
+++ vre-panel/next.config.js
@@ -8,6 +8,7 @@ module.exports = {
   assetPrefix: process.env.NODE_ENV === "production" ? '/vreapp' : '',
   publicRuntimeConfig: {
     basePath: process.env.NODE_ENV === "production" ? '/vreapp' : '',
-    staticFolder: process.env.NODE_ENV === "production" ? '/vreapp' : ''
+    staticFolder: process.env.NODE_ENV === "production" ? '/vreapp' : '',
+    NEXT_PUBLIC_ENV_VRE_API_URL: process.env.NEXT_PUBLIC_ENV_VRE_API_URL
   }
 }

da9bd3ed3ae7bcf33c759fa0d42046460267986e
skoulouzis <koulouzis@gmail.com>
2023-08-09 14:27:13 +0200

    ALLOWED_HOSTS

--- vreapis/vreapis/settings/production.py
+++ vreapis/vreapis/settings/production.py
@@ -5,7 +5,7 @@ from vreapis.settings.base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOST')]
+ALLOWED_HOSTS = ['localhost','127.0.0.1',[os.getenv('ALLOWED_HOST')]]
 CSRF_TRUSTED_ORIGINS = [os.getenv('TRUSTED_ORIGINS')]
 
 CORS_ALLOWED_ORIGIN_REGEXES = [

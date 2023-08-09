5ab8bddae774f3102d1ed439e20f7beda7289093
skoulouzis <koulouzis@gmail.com>
2023-08-09 13:59:00 +0200

    marge with add BASE_PATH variable for vreapi

--- vreapis/entrypoint.dev.sh
+++ vreapis/entrypoint.dev.sh
@@ -3,7 +3,7 @@
 APP_PORT=${PORT:-8000}
 cd /app/
 /opt/venv/bin/python manage.py makemigrations
-/opt/venv/bin/python manage.py migrate
+/opt/venv/bin/python manage.py migrate --database=development
 /opt/venv/bin/python manage.py loaddata virtual_labs
 /opt/venv/bin/python manage.py createsuperuser --no-input
 /opt/venv/bin/python manage.py runserver

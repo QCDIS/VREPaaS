ae494561df8e3e627fb9053f737f1bcfd70541d9
skoulouzis <koulouzis@gmail.com>
2023-08-09 14:18:23 +0200

    set development DB

--- vreapis/entrypoint.sh
+++ vreapis/entrypoint.sh
@@ -4,7 +4,7 @@ APP_PORT=${PORT:-8000}
 cd /app/
 /opt/venv/bin/python manage.py collectstatic --no-input
 /opt/venv/bin/python manage.py makemigrations
-/opt/venv/bin/python manage.py migrate
+/opt/venv/bin/python manage.py migrate --database=development
 /opt/venv/bin/python manage.py loaddata virtual_labs
 /opt/venv/bin/python manage.py createsuperuser --no-input
 /opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm vreapis.wsgi:application --bind "0.0.0.0:${APP_PORT}"

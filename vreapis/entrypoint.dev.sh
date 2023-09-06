e530272284f483754763ee2995d9215914c55ed0
skoulouzis <koulouzis@gmail.com>
2023-09-06 14:13:57 +0200

    added VRE_API_TOKEN var

--- vreapis/entrypoint.dev.sh
+++ vreapis/entrypoint.dev.sh
@@ -6,5 +6,5 @@ cd /app/
 /opt/venv/bin/python manage.py migrate
 /opt/venv/bin/python manage.py loaddata virtual_labs workflows catalogs data_products
 /opt/venv/bin/python manage.py createsuperuser --no-input
-/opt/venv/bin/python manage.py create_custom_token n-a-a-vre
+/opt/venv/bin/python manage.py create_custom_token n-a-a-vre --token_env_var VRE_API_TOKEN
 /opt/venv/bin/python manage.py runserver 0.0.0.0:8000
--- vreapis/workflows/management/commands/create_custom_token.py
+++ vreapis/workflows/management/commands/create_custom_token.py
@@ -11,12 +11,18 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('username', type=str, help='Username of the user')
+        parser.add_argument('--token_env_var', type=str, help='Name of the environment variable containing the token')
 
     def handle(self, *args, **options):
 
         self.stdout.write(self.style.ERROR('------------------------------Create Custom Token------------------------------'))
         username = options['username']
         password = os.environ.get("DJANGO_USER_PASSWORD", default="change_me_please")
+        token_env_var = options['token_env_var']
+        self.stdout.write(
+            self.style.ERROR('token_env_var: '+token_env_var))
+        token_key = os.environ.get(token_env_var)
+
         User = get_user_model()
         if not User.objects.filter(username=options['username']).exists():
             self.stdout.write(f"User {options['username']} not present, creating it ...")
@@ -24,9 +30,13 @@ class Command(BaseCommand):
             user.set_password(password)
             user.save()
             self.stdout.write(f"user {options['username']} created!")
-        token, created = Token.objects.get_or_create(user=user)
-        self.stdout.write(self.style.SUCCESS(f'Token created for user: {username}'))
-        self.stdout.write(self.style.SUCCESS(f'Token key: {token.key}'))
+
+        if token_key:
+            token, created = Token.objects.get_or_create(user=user, key=token_key)
+            self.stdout.write(self.style.SUCCESS(f'Token created for user: {username}'))
+            self.stdout.write(self.style.SUCCESS(f'Token key: {token.key}'))
+        else:
+            self.stdout.write(self.style.ERROR('Token environment variable not found.'))
 
 
 

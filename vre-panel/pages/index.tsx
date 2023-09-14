266403f7da753f85f469f42fcc6dc6153a095a84
Gabriel Pelouze <gabriel@pelouze.net>
2023-09-14 13:50:50 +0200

    cleanup and reformat index

--- vre-panel/pages/index.tsx
+++ vre-panel/pages/index.tsx
@@ -1,106 +1,70 @@
-import { getToken } from 'next-auth/jwt';
-// import { signIn, useSession } from 'next-auth/react';
+import {useEffect, useState} from 'react';
 import getConfig from 'next/config';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import { NewVREDialog } from '../components/NewVREDialog';
-import { Nav } from '../templates/Nav';
-// import useAuth from './auth/useAuth';
+
+import {NewVREDialog} from '../components/NewVREDialog';
+import {Nav} from '../templates/Nav';
 
 const getSlug = (title: string) => {
 
-    return title
-        .toLowerCase()
-        .replace(/ /g, '-')
-        .replace(/[^\w-]+/g, '');
+  return title
+    .toLowerCase()
+    .replace(/ /g, '-')
+    .replace(/[^\w-]+/g, '');
 }
 
 const VLabs = () => {
 
-    const { publicRuntimeConfig } = getConfig()
+  const {publicRuntimeConfig} = getConfig()
 
-    // const isAuthenticated = useAuth(true);
-    const [isOpen, setIsOpen] = useState(false);
-    const [vlabs, setVlabs] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [vlabs, setVlabs] = useState([]);
 
-    useEffect(() => {
+  useEffect(() => {
 
-        // if (isAuthenticated) {
+    const apiUrl = `${window.location.origin}/${publicRuntimeConfig.apiBasePath}`
+    fetch(`${apiUrl}/vlabs`)
+      .then((res) => res.json())
+      .then((data) => {
+        setVlabs(data);
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+  }, []);
 
-            // var requestOptions: RequestInit = {
-            //     method: "GET",
-            //     headers: {
-            //         "Authorization": "Bearer: " + token.accessToken
-            //     },
-            // };
-            const apiUrl = `${window.location.origin}/${publicRuntimeConfig.apiBasePath}`
-            fetch(`${apiUrl}/vlabs`)
-                .then((res) => res.json())
-                .then((data) => {
-                    setVlabs(data);
-                })
-                .catch((error) => {
-                    console.log(error)
-                });
-        // }
-    }, []);
-
-    // const { status } = useSession({
-    //     required: true,
-    //     onUnauthenticated() {
-    //         signIn();
-    //     },
-    // })
-
-    // if (status === "loading") {
-    //     return "Loading or not authenticated..."
-    // }
-
-    return (
-        <div>
-            <Nav />
-            <div className='min-h-screen mx-auto bg-gradient-to-b from-sky-100 to-orange-300'>
-                <div className='grid grid-cols-3'>
-                    {vlabs.map((vlab: any) => {
-                        return (
-                            <div key={getSlug(vlab.title)} className="max-w-sm rounded overflow-hidden shadow-lg bg-white m-10">
-                                <Link
-                                    href={{
-                                        pathname: '/vlabs/[slug]',
-                                        query: { slug: vlab.slug }
-                                    }}
-                                >
-                                    <div>
-                                        <img className="w-35 h-30 object-cover" src={`${publicRuntimeConfig.staticFolder}/HP-VRES.png`} />
-                                        <div className="font-bold text-l mb-2 bg-blue-500 text-white p-5">{vlab.title}</div>
-                                        <div className="px-3 py-2">
-                                            <p className="text-gray-700 text-base truncate ...">
-                                                {vlab.description}
-                                            </p>
-                                        </div>
-                                    </div>
-                                </Link>
-                            </div>
-                        );
-                    })}
-                </div>
-                <NewVREDialog isOpen={isOpen} setIsOpen={setIsOpen} />
-            </div>
-        </div >
-    )
+  return (
+    <div>
+      <Nav/>
+      <div className='min-h-screen mx-auto bg-gradient-to-b from-sky-100 to-orange-300'>
+        <div className='grid grid-cols-3'>
+          {vlabs.map((vlab: any) => {
+            return (
+              <div key={getSlug(vlab.title)} className="max-w-sm rounded overflow-hidden shadow-lg bg-white m-10">
+                <Link
+                  href={{
+                    pathname: '/vlabs/[slug]',
+                    query: {slug: vlab.slug}
+                  }}
+                >
+                  <div>
+                    <img className="w-35 h-30 object-cover" src={`${publicRuntimeConfig.staticFolder}/HP-VRES.png`}/>
+                    <div className="font-bold text-l mb-2 bg-blue-500 text-white p-5">{vlab.title}</div>
+                    <div className="px-3 py-2">
+                      <p className="text-gray-700 text-base truncate ...">
+                        {vlab.description}
+                      </p>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+        <NewVREDialog isOpen={isOpen} setIsOpen={setIsOpen}/>
+      </div>
+    </div>
+  )
 };
-
-export async function getServerSideProps(context:any) {
-
-    const { req } = context;
-    const secret = process.env.SECRET;
-    const token = await getToken({ req, secret });
-
-    return {
-        props: {
-            token: token
-        }
-    };
-}
 
 export default VLabs;

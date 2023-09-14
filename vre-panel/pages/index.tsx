b7d0099f06070978be0fe62f63d4a862f6d8bfb8
Gabriel Pelouze <gabriel@pelouze.net>
2023-09-14 14:30:50 +0200

    add loading virtual labs status

--- vre-panel/pages/index.tsx
+++ vre-panel/pages/index.tsx
@@ -19,6 +19,7 @@ const VLabs = () => {
 
   const [isOpen, setIsOpen] = useState(false);
   const [vlabs, setVlabs] = useState([]);
+  const [vlabsLoading, setVlabsLoading] = useState(true);
 
   useEffect(() => {
 
@@ -27,9 +28,11 @@ const VLabs = () => {
       .then((res) => res.json())
       .then((data) => {
         setVlabs(data);
+        setVlabsLoading(false)
       })
       .catch((error) => {
         console.log(error)
+        setVlabsLoading(false)
       });
   }, []);
 
@@ -38,28 +41,53 @@ const VLabs = () => {
       <Nav/>
       <div className='min-h-screen mx-auto bg-gradient-to-b from-sky-100 to-orange-300'>
         <div className='grid grid-cols-3'>
-          {vlabs.map((vlab: any) => {
-            return (
-              <div key={getSlug(vlab.title)} className="max-w-sm rounded overflow-hidden shadow-lg bg-white m-10">
-                <Link
-                  href={{
-                    pathname: '/vlabs/[slug]',
-                    query: {slug: vlab.slug}
-                  }}
-                >
-                  <div>
-                    <img className="w-35 h-30 object-cover" src={`${publicRuntimeConfig.staticFolder}/HP-VRES.png`}/>
-                    <div className="font-bold text-l mb-2 bg-blue-500 text-white p-5">{vlab.title}</div>
-                    <div className="px-3 py-2">
-                      <p className="text-gray-700 text-base truncate ...">
-                        {vlab.description}
-                      </p>
-                    </div>
-                  </div>
-                </Link>
+          {vlabsLoading ? (
+            <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white m-10 animate">
+              <div>
+                <img className="w-35 h-30 object-cover" src={`${publicRuntimeConfig.staticFolder}/HP-VRES.png`}/>
+                <div className="font-bold text-l mb-2 bg-gray-300 text-white p-5">
+                  <p className="animate-pulse">
+                    <span className="inline-block min-h-[1em] w-6/12 flex-auto cursor-wait bg-current align-middle opacity-50"></span>
+                  </p>
+                </div>
+                <div className="px-3 py-2">
+                  <p className="text-gray-700 text-base truncate animate-pulse ...">
+                    <span className="inline-block min-h-[0.6em] w-6/12 flex-auto cursor-wait bg-current align-middle opacity-50"></span>
+                  </p>
+                </div>
               </div>
-            );
-          })}
+            </div>
+            ) : (
+              vlabs.length > 0 ? (
+                vlabs.map((vlab: any) => {
+                  return (
+                    <div key={getSlug(vlab.title)} className="max-w-sm rounded overflow-hidden shadow-lg bg-white m-10">
+                      <Link
+                        href={{
+                          pathname: '/vlabs/[slug]',
+                          query: {slug: vlab.slug}
+                        }}
+                      >
+                        <div>
+                          <img className="w-35 h-30 object-cover" src={`${publicRuntimeConfig.staticFolder}/HP-VRES.png`}/>
+                          <div className="font-bold text-l mb-2 bg-blue-500 text-white p-5">{vlab.title}</div>
+                          <div className="px-3 py-2">
+                            <p className="text-gray-700 text-base truncate ...">
+                              {vlab.description}
+                            </p>
+                          </div>
+                        </div>
+                      </Link>
+                    </div>
+                  );
+                })
+              ) : (
+                <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white m-10 p-5 text-gray-700">
+                  Could not load virtual found
+                </div>
+              )
+            )
+          }
         </div>
         <NewVREDialog isOpen={isOpen} setIsOpen={setIsOpen}/>
       </div>

4f6ac3f05cee7aaaa9160d2e2a63159a66db3599
Gabriel Pelouze <gabriel@pelouze.net>
2023-09-19 10:33:39 +0200

    set font

--- vre-panel/tailwind.config.js
+++ vre-panel/tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './templates/**/*.{js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Titillium Web', 'sans-serif'],
+      },
+    },
   },
   variants: {
     textColor: ['responsive', 'hover', 'focus', 'active']

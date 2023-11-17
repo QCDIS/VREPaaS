f5dc954e60fd5f0f2ff0e1a86971805d7886fc59
Gabriel Pelouze <gabriel@pelouze.net>
2023-11-17 16:59:00 +0100

    fix django-keycloak-auth import

--- vreapis/vreapis/settings/base.py
+++ vreapis/vreapis/settings/base.py
@@ -75,7 +75,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django-keycloak-auth.middleware.KeycloakMiddleware',
+    'django_keycloak_auth.middleware.KeycloakMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',

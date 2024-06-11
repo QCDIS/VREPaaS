fe4c9d2d64692772889f1fd6c8d841d91f3ae7bd
AndyBRoswell <andyroswellrr@gmail.com>
2024-06-12 00:32:33 +0200

    migrating TypesHandler, BaseImageHandler, CellsHandler

--- vreapis/tests/emulated-frontend/extract.py
+++ vreapis/tests/emulated-frontend/extract.py
@@ -3,12 +3,13 @@ import json
 import os
 
 script_path: str = os.path.dirname(os.path.realpath(__file__))
-endpoint: str = f"{os.getenv('API_ENDPOINT')}/api/containerizer/extract"
+endpoint: str = f"{os.getenv('API_ENDPOINT')}/api/containerizer/extract/"
+headers: dict = {"Authorization": f"Token {os.getenv('NAAVRE_API_TOKEN')}"}
 session = requests.Session()
 
 with open(f'{script_path}/dat/extract.json') as f:
     bodies: list[dict[str, any]] = json.load(f)
 
 for body in bodies:
-    response = session.post(endpoint, body, verify=False)
-    print(response.json())
+    response = session.post(endpoint, body, headers=headers, verify=False)
+    print(response.text)

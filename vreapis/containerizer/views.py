5fbb1f9409c04d3f6ff85ab11b4514d646c08f08
Andy Bayer Roswell <andyroswellrr@gmail.com>
2024-08-27 17:28:38 +0200

    envvar check for github workflow

--- vreapis/containerizer/views.py
+++ vreapis/containerizer/views.py
@@ -190,6 +190,7 @@ class ExtractorHandler(APIView):
 
 
 class CellsHandler(viewsets.ModelViewSet):
+    common.logger.debug(f"CELL_GITHUB: {os.getenv('CELL_GITHUB')}")
     queryset: QuerySet = Cell.objects.all()
     serializer_class = CellSerializer
     authentication_classes: list[BaseAuthentication] = [StaticTokenAuthentication]

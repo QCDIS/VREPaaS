4ab0e594188b962edd09a97371988edea9a2768e
Andy Bayer Roswell <andyroswellrr@gmail.com>
2024-12-05 01:41:41 +0100

    adding support for export in legacy db schema [for the old tinydb]

--- vreapis/catalog/management/commands/export_in_legacy_db_schema.py
+++ vreapis/catalog/management/commands/export_in_legacy_db_schema.py
@@ -1,11 +1,16 @@
+import json
+import os.path
+
 from django.core.management import CommandParser
 from django.core.management.base import BaseCommand, CommandError
+from django.db.models import QuerySet
+
 from catalog.models import Cell
 from catalog.serializers import CellSerializer
 
 
 class Command(BaseCommand):
-    help = 'Export designated tables in legacy db schema'
+    help: str = 'Export designated tables in legacy db schema'
 
 
     def add_arguments(self, parser: CommandParser):
@@ -15,9 +20,17 @@ class Command(BaseCommand):
         for table in options['tables']:
             match table:
                 case 'Cell':
-                    queryset = Cell.objects.all()
+                    queryset: QuerySet = Cell.objects.all()
+                    no: int = 1
+                    cells: dict[str, dict] = {}
                     for cell in queryset:
                         serializer = CellSerializer(cell)
-                        print(serializer.data)
+                        cells[str(no)] = serializer.data
+                    db_file: str = os.path.expanduser('~/NaaVRE/NaaVRE_db.json')
+                    with open(db_file) as f:
+                        db = json.load(f)
+                        db['cells'] = cells
+                    with open(db_file, 'w') as f:
+                        json.dump(db, f)
                 case _:
                     raise CommandError(f"Table {table} is not supported")

171f0bbb4cc02fff91006e4a864f8fa1581463ad
Andy Bayer Roswell <andyroswellrr@gmail.com>
2024-12-15 17:59:06 +0100

    added some handy scripts to convert some test data to ipynb/rmd

--- vreapis/tests/extract-notebook.ps1
+++ vreapis/tests/extract-notebook.ps1
@@ -5,11 +5,12 @@ param(
 New-Item -i Directory -f $output_dir
 foreach ($input_file in $input_files) {
     $root = ConvertFrom-Json (Get-Content -raw $input_file) -d 100
-    ConvertTo-Json $root.data.notebook -d 100 > $output_dir/$($input_file.BaseName).ipynb
+    $serialized_nb = ConvertTo-Json $root.data.notebook -d 100
+    $serialized_nb > $output_dir/$($input_file.BaseName).ipynb
     switch ($root.data.kernel) {
         'ipython' {}
         'IRkernel' {
-
+            $serialized_nb | jupytext --from ipynb --to Rmd > $output_dir/$($input_file.BaseName).Rmd
         }
     }
 }

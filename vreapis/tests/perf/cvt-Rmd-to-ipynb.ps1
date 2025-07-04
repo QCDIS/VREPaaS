8d61260276e559cc54866589e8b48fca53f71b55
Andy Bayer Roswell <andyroswellrr@gmail.com>
2025-07-04 19:14:44 +0200

    refined test notebook generation script

--- vreapis/tests/perf/cvt-Rmd-to-ipynb.ps1
+++ vreapis/tests/perf/cvt-Rmd-to-ipynb.ps1
@@ -16,11 +16,9 @@ param(
         mimetype = 'text/x-r-source'
         name = 'R'
         pygments_lexer = 'r'
-#        version = '4.3.3'
-        version = '4.4.1'
+        version = '4.3.3'   # keep this unchanged to let JupyterLab not adjust automatically [or it will ask to save the file, which brings impacts on performance test results]
     }
 )
-
 
 foreach ($pathname in $pathnames) {
     Write-Host -NoNewline "src: "
@@ -30,7 +28,21 @@ foreach ($pathname in $pathnames) {
     $generated_ipynb = ConvertFrom-Json (Get-Content $dst -Raw)
     $generated_ipynb.metadata | Add-Member -NotePropertyName kernelspec -NotePropertyValue $kernelspec
     $generated_ipynb.metadata | Add-Member -NotePropertyName language_info -NotePropertyValue $language_info
-    $generated_ipynb | ConvertTo-Json -Depth 100 | Set-Content $dst
+    $serialized_ipynb = ($generated_ipynb | ConvertTo-Json -Depth 100) -split '\n'
+    $serialized_ipynb | Set-Content $dst
+#    $reindented_ipynb_builder = New-Object System.Text.StringBuilder
+#    foreach ($row in $serialized_ipynb) { # adjust indentation to 1 space [JupyterLab's indentation preference]
+#        $matches = $null
+#        if ($row -match '^\s+') {
+#            [void]$reindented_ipynb_builder.Append(' ' * ($matches[0].Length / 2))
+#            [void]$reindented_ipynb_builder.Append($row.Substring($matches[0].Length))
+#        } else {
+#            [void]$reindented_ipynb_builder.Append($row)
+#        }
+#        [void]$reindented_ipynb_builder.Append("`n")
+#    }
+#    [void]$reindented_ipynb_builder.Remove($reindented_ipynb_builder.Length - 1, 1) # remove the last newline
+#    $reindented_ipynb_builder.ToString() | Set-Content $dst
     Write-Host -NoNewline "dst: "
     Write-Host -ForegroundColor Green $dst
 }
